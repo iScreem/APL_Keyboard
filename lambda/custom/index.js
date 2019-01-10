2b00ecbb56466c2d6e16c87679d2f89a0378be5b
Cameron Hiebert <goofing.off@gmail.com>
2019-01-10 14:11:23 -0500

    intitial commit

--- lambda/custom/index.js
+++ lambda/custom/index.js
@@ -7,15 +7,118 @@ const LaunchRequestHandler = {
   canHandle(handlerInput) {
     return handlerInput.requestEnvelope.request.type === 'LaunchRequest';
   },
-  handle(handlerInput) {
-    const speechText = 'Welcome to the Alexa Skills Kit, you can say hello!';
+  async handle(handlerInput) {
+    var sAttributes = await handlerInput.attributesManager.getSessionAttributes();
+    sAttributes["keyboardText"] = "";
+    const speechText = 'Welcome to the A P L Keyboard demo. Please enter your email';
+    handlerInput.attributesManager.setSessionAttributes(sAttributes);
 
-    return handlerInput.responseBuilder
+
+    if (deviceHasDisplay(handlerInput)) {
+      return handlerInput.responseBuilder
       .speak(speechText)
       .reprompt(speechText)
-      .withSimpleCard('Hello World', speechText)
+      .addDirective({
+        type: 'Alexa.Presentation.APL.RenderDocument',
+        document: require('./layout.json'),
+        datasources: {
+          "Keyboard": {
+            "type": "object",
+            "properties": {
+              "title": "Enter email",
+              "input": "email"
+            }
+          }
+        }
+      })
       .getResponse();
+    }
+    else {
+      return handlerInput.responseBuilder
+      .speak("This example is for APL Devices. Please Try on another device")
+      .getResponse();
+    }
   },
+};
+
+const TouchHandler = {
+  canHandle(handlerInput) {
+    return (handlerInput.requestEnvelope.request.type === 'Alexa.Presentation.APL.UserEvent' && handlerInput.requestEnvelope.request.arguments[0] === "keyboard");
+  },
+  async handle(handlerInput) {
+    var sAttributes = await handlerInput.attributesManager.getSessionAttributes();
+    var keyboardText = sAttributes.keyboardText;
+    var key = handlerInput.requestEnvelope.request.arguments[2];
+    var input = handlerInput.requestEnvelope.request.arguments[1];
+    var speechText = key;
+
+    switch (key) {
+      case "Back":
+        keyboardText = keyboardText.slice(0,-1);
+        break;
+      case "SPACE":
+        keyboardText = keyboardText + " ";
+        break;
+      case "Enter":
+        sAttributes[input] = keyboardText;
+        keyboardText = "";
+        switch (input) {
+          case "email":
+            input = "username";
+            speechText = " Enter user name";
+            break;
+          case "username":
+            input = "gamertag";
+            speechText = " Enter gamer tag";
+            break;
+          case "gamertag":
+            return handlerInput.responseBuilder
+              .speak("Thank you for your input")
+              .addDirective({
+                type: 'Alexa.Presentation.APL.RenderDocument',
+                document: require('./output.json'),
+                datasources: {
+                  "Keyboard": {
+                    "type": "object",
+                    "properties": {
+                      "title": keyboardText,
+                      "input": input,
+                      "email": sAttributes.email,
+                      "username": sAttributes.username,
+                      "gamertag": sAttributes.gamertag
+                    }
+                  }
+                }
+              })
+              .getResponse();
+              break;
+        }
+        break;
+      default: 
+        keyboardText = keyboardText + key;
+    }
+
+  sAttributes.keyboardText = keyboardText;
+  handlerInput.attributesManager.setSessionAttributes(sAttributes);
+
+    return handlerInput.responseBuilder
+    .speak(speechText)
+    .reprompt(speechText)
+    .addDirective({
+      type: 'Alexa.Presentation.APL.RenderDocument',
+      document: require('./layout.json'),
+      datasources: {
+        "Keyboard": {
+          "type": "object",
+          "properties": {
+            "title": keyboardText,
+            "input": input
+          }
+        }
+      }
+    })
+    .getResponse();
+  }
 };
 
 const HelloWorldIntentHandler = {
@@ -90,11 +193,25 @@ const ErrorHandler = {
   },
 };
 
+function deviceHasDisplay(handlerInput) {
+  var result = 
+  handlerInput &&
+  handlerInput.requestEnvelope && 
+  handlerInput.requestEnvelope.context &&
+  handlerInput.requestEnvelope.context.System &&
+  handlerInput.requestEnvelope.context.System.device && 
+  handlerInput.requestEnvelope.context.System.device.supportedInterfaces && 
+  handlerInput.requestEnvelope.context.System.device.supportedInterfaces.hasOwnProperty('Alexa.Presentation.APL');
+
+  return result;
+}
+
 const skillBuilder = Alexa.SkillBuilders.custom();
 
 exports.handler = skillBuilder
   .addRequestHandlers(
     LaunchRequestHandler,
+    TouchHandler,
     HelloWorldIntentHandler,
     HelpIntentHandler,
     CancelAndStopIntentHandler,
